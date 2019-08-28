23389c66253d4474d1887d84302bbb1879a28fae
monst <monster240798@gmail.com>
2019-08-28 16:53:32 +0300

    Added Swagger

--- src/main/java/com/study/sarafan/SarafanApplication.java
+++ src/main/java/com/study/sarafan/SarafanApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SarafanApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SarafanApplication.class, args);
     }
