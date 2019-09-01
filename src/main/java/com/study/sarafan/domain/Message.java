8ac2e26be2854d2ac241ad31cbd72efb64b755cc
monst <monster240798@gmail.com>
2019-09-01 13:10:55 +0300

    Created Message controller, db, custom view, db properties and some annotation

---
+++ src/main/java/com/study/sarafan/domain/Message.java
@@ -0,0 +1,73 @@
+package com.study.sarafan.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(Views.Id.class)
+    private Long id;
+
+    @JsonView(Views.IdName.class)
+    private String text;
+
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime creatingDate;
+
+    public Message() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getCreatingDate() {
+        return creatingDate;
+    }
+
+    public void setCreatingDate(LocalDateTime creatingDate) {
+        this.creatingDate = creatingDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(id, message.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
